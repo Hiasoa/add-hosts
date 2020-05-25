90097039d766873add9c76f97caae0472f09e1c0
Hiasoa <58290748+Hiasoa@users.noreply.github.com>
2020-05-25 17:45:11 +0900

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
 @echo off
 echo @echo off^&cd/d %cd%^&py main.pyw^&copy hosts C:\\Windows\\System32\\drivers\\etc\\hosts^ > launch.bat
-echo CreateObject("Shell.Application").ShellExecute "%cd%\main.bat", "", "", "runas">"editfilepermission.vbs"
+echo CreateObject("Shell.Application").ShellExecute "%cd%\launch.bat", "", "", "runas">"editfilepermission.vbs"
 editfilepermission.vbs
